abbde134e9fde5919a4088ea81ca9a89537a1ba0
moppediert <bach.nguyen@ka-raceing.de>
2023-01-01 21:15:54 +0100

    Day 7 part 1 Somewhat done

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,21 +1,15 @@
-use std::{cell::RefCell, collections::HashMap, fs, path::Path, rc::Rc};
+use std::{borrow::Borrow, cell::RefCell, collections::HashMap, fmt, fs, path::Path, rc::Rc};
 
 pub fn solve(path: &Path) -> (u32, u32) {
     let content = fs::read_to_string(path).expect("Cannot read input file");
-    // let root = Dir(
-    //     vec![
-    //         Box::new(Dir(vec![], "subdir".to_string())),
-    //         Box::new(File(2762, "subfile".to_string())),
-    //     ],
-    //     "root".to_string(),
-    // );
-    // println!("{}", root);
     let mut file_system = HashMap::<String, Rc<RefCell<File>>>::new(); // <path, file>
-    let root_dir = Rc::new(RefCell::new(dir(None, "/")));
-    let mut current_dir = Rc::clone(&root_dir);
-    file_system.insert("/".to_string(), root_dir);
-    let mut lines = content.split("\n");
-    lines.next(); // Ignore the first command, since it's (supposedly) always cd /
+
+    // Assume the first command is always `cd /`
+    let mut current_dir = Rc::new(RefCell::new(dir(None, "/")));
+    // let mut current_dir = Rc::clone(&root_dir);
+    file_system.insert("/".to_string(), Rc::clone(&current_dir));
+
+    let lines = content.split("\n").skip(1);
     for line in lines {
         let mut split = line.split_whitespace();
 
@@ -34,33 +28,47 @@ pub fn solve(path: &Path) -> (u32, u32) {
                     }
                     ".." => {
                         let current_dir_path = Rc::as_ref(&current_dir).borrow().path.clone();
-                        let split_path = current_dir_path.split("/");
+                        let split_path = current_dir_path.split_inclusive("/");
                         let new_path = if let Some(last) = split_path.last() {
+                            println!("last item = {}", last);
                             current_dir_path.strip_suffix(last).unwrap()
                         } else {
                             "/"
                         };
+                        let previous_dir = Rc::clone(&current_dir);
                         current_dir = file_system.get(new_path).unwrap().clone();
+                        // println!("going back from {} to {}",  Rc::as_ref(&previous_dir).borrow().path, Rc::as_ref(&current_dir).borrow().path);
                     }
                     some_dir => {
+                        // println!("{:#?}", &file_system);
+
+                        let full_path =
+                            Rc::as_ref(&current_dir).borrow().path.clone() + some_dir + "/";
+                        // println!("{}", full_path);
+
                         // Assume that the dir following the `cd` command already appeared in the result of one of the `ls` commands before
-                        current_dir = file_system.get(some_dir).unwrap().clone();
+                        current_dir = file_system.get(full_path.as_str()).unwrap().clone();
                     }
                 }
             }
         } else if first.starts_with("dir") {
             let dir_name = split.next().unwrap();
-            let new_dir = Rc::new(RefCell::new(dir(Some(current_dir), dir_name)));
+            let new_dir = Rc::new(RefCell::new(dir(Some(Rc::clone(&current_dir)), dir_name)));
             let new_dir_path = Rc::as_ref(&new_dir).borrow().path.clone();
             file_system.insert(new_dir_path, new_dir);
         } else {
             let file_size = first.parse::<usize>().unwrap();
             let file_name = split.next().unwrap();
-            let file = Rc::new(RefCell::new(file(Some(current_dir), file_name, file_size)));
+            let file = Rc::new(RefCell::new(file(
+                Some(Rc::clone(&current_dir)),
+                file_name,
+                file_size,
+            )));
             let file_path = Rc::as_ref(&file).borrow().path.clone();
             file_system.insert(file_path, file);
         }
     }
+    println!("{:#?}", &file_system);
 
     (0, 0)
 }
@@ -71,7 +79,7 @@ fn file(parent_dir: Option<Rc<RefCell<File>>>, name: &str, size: usize) -> File 
 
     File {
         parent: Some(Rc::clone(&parent)),
-        path: format!("{}/{}", Rc::as_ref(&parent).borrow().path, name),
+        path: format!("{}{}", Rc::as_ref(&parent).borrow().path, name),
         is_dir: false,
         name: name.to_string(),
         size,
@@ -82,7 +90,7 @@ fn dir(parent_dir: Option<Rc<RefCell<File>>>, name: &str) -> File {
     if let Some(parent) = parent_dir {
         File {
             parent: Some(Rc::clone(&parent)),
-            path: format!("{}/{}", Rc::as_ref(&parent).borrow().path, name),
+            path: format!("{}{}/", Rc::as_ref(&parent).borrow().path, name),
             is_dir: true,
             name: name.to_string(),
             size: 0,
@@ -104,6 +112,12 @@ struct File {
     is_dir: bool,
     name: String,
     size: usize,
+}
+
+impl fmt::Debug for File {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.name, self.size)
+    }
 }
 
 impl File {
